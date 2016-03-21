b41837da1d819d98d4aeb2a5bbdd0d5834ae6c3b
Peter Briggs <peter.briggs@manchester.ac.uk>
2016-03-21 13:27:15 +0000

    Set the default quota for registered users in the pal_finder instance.

---
+++ set_default_quota.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Set the default quota for registered users
+
+GALAXY_DB=$1
+GALAXY_USER=$2
+export PGPASSWORD=$3
+QUOTA_GB=$4
+
+if [ -z "$QUOTA_GB" ] ; then
+    # Quota is specifed in gigabytes
+    QUOTA_GB="5"
+fi
+
+quota_bytes=$(($QUOTA_GB * 1024 * 1024 * 1024))
+
+psql -d $GALAXY_DB -U $GALAXY_USER 1>/dev/null 2>&1 <<EOF
+--
+INSERT INTO quota
+   (id,create_time,update_time,name,description,bytes,operation,deleted)
+   SELECT 1,NOW(),NOW(),'Default','Default quota',$quota_bytes,'=',FALSE
+WHERE
+   NOT EXISTS (
+      SELECT id FROM quota WHERE id = 1
+   );
+INSERT INTO default_quota_association
+   (id,create_time,update_time,type,quota_id)
+   SELECT 1,NOW(),NOW(),'registered',1
+WHERE
+   NOT EXISTS (
+      SELECT id FROM default_quota_association WHERE id = 1
+   );
+EOF
