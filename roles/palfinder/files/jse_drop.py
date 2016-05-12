66a9b0ea37f4eac3538054104b3c8cd5b1eb95d6
Peter Briggs <peter.briggs@manchester.ac.uk>
2016-05-12 11:46:02 +0100

    Initial versions of Python API and Galaxy runner for JSE-Drop job submission.

---
+++ roles/palfinder/files/jse_drop.py
@@ -0,0 +1,345 @@
+"""
+Python API for the JSE-drop job submission mechnanism.
+"""
+import os
+import tempfile
+import re
+
+# JSE-drop job status codes
+class JSEDropStatus(object):
+    MISSING = 0
+    WAITING = 1
+    SUBMITTED = 2
+    FAILED = 3
+    RUNNING = 4
+    FINISHED = 5
+
+class JSEDrop(object):
+    """
+    Class providing API to JSE-drop job submission system
+
+    To use:
+
+    Create a new JSEDrop instance pointing to a 'drop-off'
+    directory which is used for submission/communication with
+    JSE-drop:
+
+    >>> jse = JSEDrop('/mnt/jse-drop')
+
+    Submit the contents of a script:
+
+    >>> jse.run('my_job','sleep 5\necho "Finished sleeping")
+
+    Get the id of the job as assigned by JSE-drop:
+
+    >>> jse.get_job_id('my_job')
+
+    Get the state of the job (returns a code number for the job state):
+
+    >>> jse.status('my_job')
+
+    Get the contents of qstat for the job (returns a dictionary):
+
+    >>> jse.qstat('my_job')
+
+    Get the contents of qacct for the job (returns a dictionary):
+
+    >>> jse.qcct('my_job')
+
+    Get the stdout and stderr files:
+
+    >>> jse.stdout_file('my_job')
+    >>> jse.stderr_file('my_job')
+
+    Terminate a running job:
+
+    >>> jse.kill('my_job')
+
+    """
+    def __init__(self,drop_dir):
+        """
+        Create new JSEDrop instance
+
+        Arguments:
+          drop_dir (str): path to JSE 'drop-off' directory
+
+        """
+        self._drop_dir = os.path.abspath(drop_dir)
+        if not os.path.isdir(self._drop_dir):
+            raise OSError("Missing drop dir: %s" % self._drop_dir)
+
+    @property
+    def drop_dir(self):
+        """
+        Return absolute path to JSE drop-off directory
+        """
+        return self._drop_dir
+
+    def run(self,name,script,mode=0775):
+        """
+        Submit a script to JSE-drop
+
+        Arguments:
+          name (str): name of the job (used as a handle to
+            interrogate status etc)
+          script (str): contents of script to be submitted
+            to JSE-drop
+
+        """
+        drop_file = os.path.join(self._drop_dir,"%s.drop.qsub" % name)
+        if os.path.exists(drop_file):
+            raise OSError("Job with name '%s' already exists" % name)
+        fd,tmp_drop_file = tempfile.mkstemp()
+        with open(tmp_drop_file,'w') as fp:
+            fp.write(script)
+        os.close(fd)
+        os.rename(tmp_drop_file,drop_file)
+        os.chmod(drop_file,mode)
+        return drop_file
+
+    def get_job_id(self,name):
+        """
+        Return the id assigned to the job by JSE-drop
+
+        Arguments:
+          name (str): name of the job
+
+        """
+        qsubmit_file = os.path.join(self._drop_dir,"%s.drop.qsubmit" % name)
+        if not os.path.exists(qsubmit_file):
+            return None
+        with open(qsubmit_file) as fp:
+            #//my_job  qNmoihPDDImLgWtetEZKhTSjmLhUikwg--JSE-DROP//
+            job_id = fp.read()
+        return job_id.strip().split(' ')[1].rstrip('/')
+
+    def status(self,name):
+        """
+        Return the status code for the job
+
+        Arguments:
+          name (str): name of the job
+
+        Returns:
+          Integer: status code.
+
+        """
+        base_name = os.path.join(self._drop_dir,name)
+        if not os.path.exists("%s.drop.qsub" % base_name):
+            # No submission script found
+            return JSEDropStatus.MISSING
+        if os.path.exists("%s.drop.qfail" % base_name):
+            # Submission failed
+            return JSEDropStatus.FAILED
+        if not os.path.exists("%s.drop.qsubmit" % base_name):
+            # Waiting for submission
+            return JSEDropStatus.WAITING
+        if not os.path.exists("%s.drop.qacct" % base_name):
+            # Running
+            return JSEDropStatus.RUNNING
+        # Finished
+        return JSEDropStatus.FINISHED
+
+    def qstat(self,name):
+        """
+        Return the qstat information for the job
+
+        Arguments:
+          name (str): name of the job
+
+        Returns:
+          Dictionary: dictionary where keys are items from
+            the .drop.qstat file; dictionary will be empty if no
+            .drop.qstat file was found (e.g. because the job hasn't
+            started yet).
+
+        """
+        qstat_file = os.path.join(self._drop_dir,"%s.drop.qstat" % name)
+        if not os.path.exists(qstat_file):
+            return {}
+        qstat = {}
+        with open(qstat_file) as fp:
+            #<JB_job_number>204784</JB_job_number>
+            #<JAT_prio>50.25000</JAT_prio>
+            #<JB_name>drop-test  qNmoihPDDImLgWtetEZKhTSjmLhUikwg--JSE-DROP</JB_name>
+            #<JB_owner>simonh</JB_owner>
+            #<state>r</state>
+            #<JAT_start_time>2016-04-28T17:30:19</JAT_start_time>
+            #<queue_name>C6220-galaxy.q@node009.prv.hydra.cluster</queue_name>
+            #<slots>1</slots>
+            for line in fp:
+                m = re.match(r'<([^>]+)>([^<]+)</([^>]+)>',line.rstrip('\n'))
+                if m:
+                    qstat[m.group(1)] = m.group(2)
+        return qstat
+
+    def qacct(self,name):
+        """
+        Return the qacct information for the job
+
+        Arguments:
+          name (str): name of the job
+
+        Returns:
+          Dictionary: dictionary where keys are items from
+            the .drop.qacct file; dictionary will be empty if no
+            .drop.qacct file was found (e.g. because the job hasn't
+            completed yet).
+
+        """
+        qacct_file = os.path.join(self._drop_dir,"%s.drop.qacct" % name)
+        if not os.path.exists(qacct_file):
+            return {}
+        qacct = {}
+        with open(qacct_file) as fp:
+            #==============================================================
+            #qname        C6220-galaxy.q
+            #hostname     node009.prv.hydra.cluster
+            #group        simonh
+            #owner        simonh
+            #project      NONE
+            #department   defaultdepartment
+            #jobname      drop-test  qNmoihPDDImLgWtetEZKhTSjmLhUikwg--JSE-DROP
+            #jobnumber    204784
+            #taskid       undefined
+            #account      sge
+            #priority     0
+            #qsub_time    Thu Apr 28 17:30:19 2016
+            #start_time   Thu Apr 28 17:30:19 2016
+            #end_time     Thu Apr 28 17:32:19 2016
+            #granted_pe   NONE
+            #slots        1
+            #failed       0
+            #exit_status  0
+            #...
+            for line in fp:
+                try:
+                    key,value = [x.strip() for x in line.split(' ',1)]
+                    qacct[key] = value
+                except ValueError:
+                    pass
+        return qacct
+
+    def qfail(self,name):
+        """
+        Return the qfail information for the job
+
+        Arguments:
+          name (str): name of the job
+
+        Returns:
+          Dictionary: dictionary where keys are items from
+            the .drop.qfail file; dictionary will be empty if no
+            .drop.qfail file was found (e.g. because the job didn't
+            fail on submission).
+
+        """
+        qfail_file = os.path.join(self._drop_dir,"%s.drop.qfail" % name)
+        if not os.path.exists(qfail_file):
+            return {}
+        qfail = {}
+        with open(qfail_file) as fp:
+            #======================================
+            #
+            #Exit status: //2//
+            #
+            #STDOUT:
+            #
+            #
+            #STDERR:
+            #Unable to read script file because of error: ERROR! -pe option must have range as 2nd argument
+            #
+            #
+            #======================================
+            section = None
+            for line in fp:
+                if line == "======================================\n":
+                    continue
+                if line.startswith('Exit status:'):
+                    qfail['exit_code'] = line[:-1].split()[-1].strip('/')
+                    continue
+                if line.startswith('STDOUT:'):
+                    section = 'stdout'
+                    qfail['stdout'] = ''
+                    continue
+                elif line.startswith('STDERR:'):
+                    section = 'stderr'
+                    qfail['stderr'] = ''
+                    continue
+                if section is not None:
+                    qfail[section] += line
+        return qfail
+
+    def stdout_file(self,name):
+        """
+        Return path to the stdout file for the job
+
+        Constructs and returns the full path to the stdout
+        file for the job, based on information from the
+        .drop.qacct file.
+
+        If no information could be read from .drop.qacct
+        (e.g. job hasn't finished yet) then returns None.
+
+        There is no guarantee that the named file exists.
+
+        """
+        qacct = self.qacct(name)
+        if not qacct:
+            return None
+        return os.path.join(self._drop_dir,
+                            '%s.o%s' % (qacct['jobname'],
+                                        qacct['jobnumber']))
+
+    def stderr_file(self,name):
+        """
+        Return path to the stderr file for the job
+
+        Constructs and returns the full path to the stderr
+        file for the job, based on information from the
+        .drop.qacct file.
+
+        If no information could be read from .drop.qacct
+        (e.g. job hasn't finished yet) then returns None.
+
+        There is no guarantee that the named file exists.
+
+        """
+        qacct = self.qacct(name)
+        if not qacct:
+            return None
+        return os.path.join(self._drop_dir,
+                            '%s.e%s' % (qacct['jobname'],
+                                        qacct['jobnumber']))
+
+    def kill(self,name):
+        """
+        Terminate a running job
+
+        Arguments:
+          name (str): name of the job
+
+        """
+        raise NotImplementedError("JSEDrop.kill not implemented")
+
+if __name__ == '__main__':
+    # Test program
+    import sys
+    try:
+        job_name = sys.argv[1]
+    except Exception:
+        sys.stderr.write("Need to supply job name")
+        sys.exit(1)
+    jse = JSEDrop('.')
+    status = jse.status(job_name)
+    print "Job   : %s" % job_name
+    print "Status: %s" % status
+    if status == JSEDropStatus.RUNNING:
+        print "Job is running"
+        print "State: %s" % jse.qstat(job_name)['state']
+    elif status == JSEDropStatus.FINISHED:
+        print "Job has finished"
+        print "Exit status: %s" % jse.qacct(job_name)['exit_status']
+    elif status == JSEDropStatus.FAILED:
+        print "Job has failed"
+        print "Exit status: %s" % jse.qfail(job_name)['exit_code']
---
+++ roles/palfinder/files/jse_drop_runner.py
@@ -0,0 +1,232 @@
+"""
+Job runner used to execute Galaxy jobs through JSE-drop.
+"""
+
+import logging
+import string
+import time
+
+from galaxy import model
+from galaxy.jobs.runners import AsynchronousJobRunner
+from galaxy.jobs.runners import AsynchronousJobState
+
+from galaxy.jobs.runners.jse_drop import JSEDrop
+from galaxy.jobs.runners.jse_drop import JSEDropStatus
+
+log = logging.getLogger( __name__ )
+
+__all__ = [ 'JSEDropJobRunner' ]
+
+class JSEDropJobRunner(AsynchronousJobRunner):
+    """
+    Job runner dropping job files into shared dir for execution by JSE
+    """
+    runner_name = "JSEDropJobRunner"
+
+    def __init__(self,app,nworkers,**kwargs):
+        """
+        Start the job runner
+        """
+        # Initialise the base class
+        super( JSEDropJobRunner, self ).__init__( app, nworkers, **kwargs )
+        # Initialise the queues from the base class
+        self._init_monitor_thread()
+        self._init_worker_threads()
+
+    def _get_job_name(self,galaxy_id_tag,tool_id=None):
+        """
+        Return a unique job id for JSE-Drop
+        """
+        job_name = 'g%s' % galaxy_id_tag
+        if tool_id:
+            job_name += '_%s' % tool_id
+        job_name = ''.join(map(lambda x:
+                               x if x in (string.letters+string.digits+'_')
+                               else '_', job_name))
+        return job_name
+
+    def _get_drop_dir(self,job_destination):
+        """
+        Extract drop-off directory from job destination parameters
+        """
+        return job_destination.params.get('dropDir',None)
+
+    def parse_destination_params(self, params):
+        """Parse the JobDestination ``params`` dict and return the runner's native representation of those params.
+        """
+        raise NotImplementedError("JSEDropJobRunner: 'parse_destination_params' not implemented")
+
+    def queue_job(self, job_wrapper):
+        """Write JSE-Drop file to drop location
+        """
+        # Get the configured job destination
+        job_destination = job_wrapper.job_destination
+        # Get the parameters defined for this destination
+        # i.e. location of the drop-off directory etc
+        drop_off_dir = self._get_drop_dir(job_destination)
+        log.debug("queue_job: drop-off dir = %s" % drop_off_dir)
+        jse_drop = JSEDrop(drop_off_dir)
+        # ID and name for job
+        galaxy_id_tag = job_wrapper.get_id_tag()
+        log.debug("ID tag: %s" % galaxy_id_tag)
+        job_name = self._get_job_name(galaxy_id_tag,
+                                      job_wrapper.tool.old_id)
+        log.debug("Job name: %s" % job_name)
+        # Prepare the job wrapper (or abort)
+        if not self.prepare_job(job_wrapper):
+            return
+        # Create script contents
+        script = self.get_job_file(job_wrapper,
+                                   exit_code_path=None)
+        # Separate leading shell specification from generated script
+        shell = '\n'.join(filter(lambda x: x.startswith('#!'),
+                                 script.split('\n')))
+        script = '\n'.join(filter(lambda x: not x.startswith('#!'),
+                                  script.split('\n')))
+        # Create header with embedded qsub flags
+        qsub_header = '\n'.join(("#$ -V",
+                                 "#$ -wd %s" % job_wrapper.working_directory))
+        log.debug("qsub_header: %s" % qsub_header)
+        # Reassemble the script components
+        script = "\n".join((shell,qsub_header,script))
+        # Create the drop file to submit the job
+        try:
+            drop_file = jse_drop.run(job_name,script)
+            log.debug("created drop file %s" % drop_file)
+            log.info("(%s) submitted as %s" % (galaxy_id_tag,job_name))
+        except:
+            # Some problem writing the qsub file
+            job_wrapper.fail("failure preparing job script",
+                             exception=True )
+            log.exception("(%s/%s) failure writing job script" %
+                          (galaxy_id_tag,job_name))
+            return
+        # External job id (i.e. id used by JSE-Drop as a handle to
+        # identify the job) is the same as the job name here
+        external_job_id = job_name
+        # Store runner information for tracking if Galaxy restarts
+        job_wrapper.set_job_destination(job_destination,
+                                        external_job_id)
+        # Store state information for job
+        job_state = AsynchronousJobState()
+        job_state.job_wrapper = job_wrapper
+        job_state.job_id = job_name
+        job_state.old_state = True
+        job_state.running = False
+        job_state.job_destination = job_destination
+        # Add to the queue of jobs to monitor
+        self.monitor_job(job_state)
+
+    def stop_job(self,job):
+        # Attempts to remove a job from the JSE-Drop queue
+        # Fetch the job id used by JSE-Drop
+        job_name = job.get_job_runner_external_id()
+        # Fetch the drop dir
+        try:
+            drop_off_dir = job.destination_params['dropDir']
+            log.debug("stop_job: drop-off dir = %s" % drop_off_dir)
+            jse_drop = JSEDrop(drop_off_dir)
+            # Delete the job
+            jse_drop.kill(job_name)
+            log.debug("JSE-drop: killed job '%s'" % job_name)
+        except Exception,ex:
+            log.error("stop_job: failed with exception: %s", ex)
+
+    def recover(self,job,job_wrapper):
+        # Recovers jobs in the queued/running state when Galaxy started
+        # What is 'job' an instance of???
+        # Could be model.Job?
+        # Fetch the job id used by JSE-Drop
+        job_name = job.get_job_runner_external_id()
+        # Get the job destination
+        job_destination = job_wrapper.job_destination
+        # Fetch the drop dir
+        drop_off_dir = self._get_drop_dir(job_destination)
+        log.debug("recover: drop-off dir = %s" % drop_off_dir)
+        jse_drop = JSEDrop(drop_off_dir)
+        # Store state information for job
+        job_state = AsynchronousJobState()
+        job_state.job_wrapper = job_wrapper
+        job_state.job_id = job_name
+        job_state.job_destination = job_destination
+        # Sort out the status
+        if job.state == model.Job.states.RUNNING:
+            job_state.old_state = True
+            job_state.running = True
+        elif job.get_state() == model.Job.states.QUEUED:
+            job_state.old_state = True
+            job_state.running = False
+        # Add to the queue of jobs to monitor
+        self.monitor_queue.put(job_state)
+
+    def check_watched_item(self,job_state):
+        # Return updated job state
+        drop_off_dir = self._get_drop_dir(job_state.job_destination)
+        jse_drop = JSEDrop(drop_off_dir)
+        job_name = job_state.job_id
+        jse_drop_status = jse_drop.status(job_name)
+        if jse_drop_status == JSEDropStatus.FINISHED:
+            # Mark as finished
+            self.mark_as_finished(job_state)
+            return None
+        if jse_drop_status in (JSEDropStatus.FAILED,
+                               JSEDropStatus.MISSING):
+            if jse_drop_status == JSEDropStatus.FAILED:
+                # Get message from qfail file
+                message = "Submission to JSE-drop failed: %s" % \
+                          jse_drop.qfail(job_name)['stderr']
+            else:
+                # Can't find the job
+                message = "%s: no such job in JSE-drop?" % job_name
+            self.fail_job(job_state,message)
+            return None
+        if jse_drop_status == JSEDropStatus.RUNNING and not job_state.running:
+            # Job started running
+            job_state.running = True
+            job_state.job_wrapper.change_state(model.Job.states.RUNNING)
+        return job_state
+
+    def finish_job(self,job_state):
+        # Looks like we need to over-ride the version of this
+        # method in the base class in order to handle setting of
+        # stdout/stderr files and exit code
+        galaxy_id_tag = job_state.job_wrapper.get_id_tag()
+        external_job_id = job_state.job_id
+        # Initialise JSE-drop
+        drop_off_dir = self._get_drop_dir(job_state.job_destination)
+        log.debug("stop_job: drop-off dir = %s" % drop_off_dir)
+        jse_drop = JSEDrop(drop_off_dir)
+        # Get stdout, stderr and exit code
+        # Nb qacct info might not be available immediately
+        trynum = 0
+        while trynum < 5:
+            try:
+                exit_code = int(jse_drop.qacct(external_job_id)['exit_status'])
+                break
+            except KeyError:
+                trynum += 1
+                log.warning("finish_job %s: unable to get exit code, retrying"
+                            % external_job_id)
+                time.sleep(5)
+        else:
+            # Failed to get the exit code so fail the job
+            log.error("finish_job %s: failed to get exit code"
+                      % external_job_id)
+            job_state.job_wrapper.fail("JSE-drop error: unable to get exit code")
+            return
+        # Read contents of stdout and stderr
+        with open(jse_drop.stdout_file(external_job_id)) as fp:
+            stdout = fp.read()
+        with open(jse_drop.stderr_file(external_job_id)) as fp:
+            stderr = fp.read()
+        # clean up the job files
+        cleanup_job = self.app.config.cleanup_job
+        if cleanup_job == "always" or (not stderr and cleanup_job == "onsuccess"):
+            job_state.cleanup()
+        # Set the job state
+        try:
+            job_state.job_wrapper.finish(stdout,stderr,exit_code)
+        except:
+            log.exception("(%s/%s) Job wrapper finish method failed" %
+                          (galaxy_id_tag,external_job_id ))
+            job_state.job_wrapper.fail("Unable to finish job", exception=True)
