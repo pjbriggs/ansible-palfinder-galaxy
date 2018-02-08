e74d7ff8a708025aff9cc1283efa12c022efb833
Peter Briggs <peter.briggs@manchester.ac.uk>
2018-02-08 16:23:52 +0000

    Update the 'delete_old_datasets.sh' script to write date/time etc to stdout.

--- roles/galaxy-utils/files/delete_old_datasets.sh
+++ roles/galaxy-utils/files/delete_old_datasets.sh
@@ -2,6 +2,9 @@
 
 # Flag all 'old' datasets in the Galaxy database as deleted
 
+echo "# $(basename $0)"
+echo "# Started $(date 2>&1)"
+
 GALAXY_DB=$1
 GALAXY_USER=$2
 export PGPASSWORD=$3
@@ -11,7 +14,9 @@ if [ -z "$INTERVAL" ] ; then
     INTERVAL="30 days"
 fi
 
-psql -d $GALAXY_DB -U $GALAXY_USER 1>/dev/null 2>&1 <<EOF
+psql -d $GALAXY_DB -U $GALAXY_USER 2>&1 <<EOF
 --
 UPDATE history_dataset_association SET deleted = TRUE WHERE create_time < (NOW() - INTERVAL '$INTERVAL') AND deleted = FALSE AND history_id IN (SELECT history.id FROM history JOIN galaxy_user ON history.user_id = galaxy_user.id WHERE username != 'admin');
 EOF
+
+echo "# Finished $(date 2>&1)"
