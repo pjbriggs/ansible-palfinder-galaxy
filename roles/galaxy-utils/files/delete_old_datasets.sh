70f784ea6cb0e61545138a7872e83bbb37bd0852
Peter Briggs <peter.briggs@manchester.ac.uk>
2018-02-12 16:08:16 +0000

    delete_old_datasets.sh: add more substantial comments explaining what it does.

--- roles/galaxy-utils/files/delete_old_datasets.sh
+++ roles/galaxy-utils/files/delete_old_datasets.sh
@@ -1,6 +1,28 @@
 #!/bin/sh
 
 # Flag all 'old' datasets in the Galaxy database as deleted
+#
+# Usage: delete_old_datasets.sh GALAXY_DB GALAXY_DB_USER DB_PASSWD [ INTERVAL ]
+#
+# Example: delete_old_datasets.sh galaxy_db galaxy MyPass "28 days"
+#
+# What it does:
+#
+# This script looks for history dataset associations (HDAs) which are
+# older than the specified time interval (measured from the creation time
+# of the HDA) and which have not already been marked as 'deleted', and
+# which don't belong to the 'admin' user.
+# Each matching HDA is then marked as 'deleted'.
+#
+# Once the HDAs have been checked, the script looks for datasets which
+# haven't already been deleted, but where the HDA is marked as deleted
+# (and where there are no undeleted library dataset associations i.e.
+# LDDAs).
+# Each matching dataset is then also marked as 'deleted'.
+#
+# Subsequently running the delete_datasets.sh/purge_datasets.sh scripts
+# from Galaxy should be sufficient to remove any associated data files
+# from disk (see https://galaxyproject.org/admin/config/performance/purge-histories-and-datasets/)
 
 echo "# $(basename $0)"
 echo "# Started $(date 2>&1)"
