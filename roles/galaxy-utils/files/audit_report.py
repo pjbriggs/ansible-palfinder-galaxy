5547dedd01fda46077135192b1f550096c5a50e7
Peter Briggs <peter.briggs@manchester.ac.uk>
2018-07-05 09:55:10 +0100

    Update audit_report.py to get settings from galaxy.yml or galaxy.ini.

--- roles/galaxy-utils/files/audit_report.py
+++ roles/galaxy-utils/files/audit_report.py
@@ -6,44 +6,128 @@ import os
 import optparse
 from ConfigParser import ConfigParser
 from ConfigParser import NoOptionError
+import yaml
 import psycopg2
 import logging
 import smtplib
 from email.mime.text import MIMEText
 import time
 
-def users(cur,interval=None,active=None):
-    # Get information on users
-    select = "SELECT email FROM galaxy_user"
-    where = []
-    if active is not None:
-        if active:
-            where.append("active = TRUE")
+class GalaxyConfig(object):
+    def __init__(self,galaxy_conf_file):
+        # only handles .ini config file
+        if galaxy_conf_file.endswith(".ini"):
+            self._format = "ini"
+            self._config = ConfigParser()
+            self._config.read(galaxy_conf_file)
+        elif galaxy_conf_file.endswith(".yml"):
+            self._format = "yaml"
+            self._config = yaml.load(open(galaxy_conf_file,'r').read())
         else:
-            where.append("active = FALSE")
-    if interval is not None:
-        where.append("create_time >= (NOW() - INTERVAL '%s')" % interval)
-    sql = select
-    if where:
-        sql += ' WHERE ' + ' AND '.join(where)
-    logging.debug(sql)
-    cur.execute(sql)
-    return cur.fetchall()
+            raise NotImplementedError("'%s': unrecognised format"
+                                      % galaxy_conf_file)
+    def _get(self,section,key):
+        if self.format == "ini":
+            try:
+                return self._config.get(section,key)
+            except NoOptionError:
+                logging.error("'%s' not found in '%s'" % (section,key))
+                return None
+        elif self.format == "yaml":
+            try:
+                return self._config[section][key]
+            except KeyError:
+                logging.error("'%s' not found in '%s'" % (section,key))
+                return None
 
-def jobs(cur,interval=None,state=None):
-    # Get information on jobs
-    select = "SELECT id,state,exit_code FROM job"
-    where = []
-    if state is not None:
-        where.append("state = '%s'" % state)
-    if interval is not None:
-        where.append("update_time >= (NOW() - INTERVAL '%s')" % interval)
-    sql = select
-    if where:
-        sql += ' WHERE ' + ' AND '.join(where)
-    logging.debug(sql)
-    cur.execute(sql)
-    return cur.fetchall()
+    def galaxy_setting(self,param):
+        if self.format == "ini":
+            section = "app:main"
+        elif self.format == "yaml":
+            section = "galaxy"
+        return self._get(section,param)
+
+    @property
+    def format(self):
+        return self._format
+
+    @property
+    def database_connection(self):
+        return self.galaxy_setting('database_connection')
+
+    @property
+    def brand(self):
+        return self.galaxy_setting('brand')
+
+    @property
+    def smtp_server(self):
+        return self.galaxy_setting('smtp_server')
+
+    @property
+    def email_from(self):
+        return self.galaxy_setting('email_from')
+
+class GalaxyDatabase(object):
+    def __init__(self,galaxy_conf_file):
+        # Get configuration
+        self._config = GalaxyConfig(galaxy_conf_file)
+        # Connect to database
+        name,user,passwd = split_db_connection(
+            self._config.database_connection)
+        self._conn = psycopg2.connect(
+            database=name,
+            user=user,
+            password=passwd)
+        self._cur = self._conn.cursor()
+
+    def close(self):
+        self._conn.close()
+
+    @property
+    def config(self):
+        # Return GalaxyConfig instance
+        return self._config
+
+    def users(self,interval=None,active=None):
+        # Get information on users
+        select = "SELECT email FROM galaxy_user"
+        where = []
+        if active is not None:
+            if active:
+                where.append("active = TRUE")
+            else:
+                where.append("active = FALSE")
+        if interval is not None:
+            where.append("create_time >= (NOW() - INTERVAL '%s')"
+                         % interval)
+        sql = select
+        if where:
+            sql += ' WHERE ' + ' AND '.join(where)
+        logging.debug(sql)
+        self._cur.execute(sql)
+        return self._cur.fetchall()
+
+    def jobs(self,interval=None,state=None):
+        # Get information on jobs
+        select = "SELECT id,state,exit_code FROM job"
+        where = []
+        if state is not None:
+            where.append("state = '%s'" % state)
+        if interval is not None:
+            where.append("update_time >= (NOW() - INTERVAL '%s')"
+                         % interval)
+        sql = select
+        if where:
+            sql += ' WHERE ' + ' AND '.join(where)
+        logging.debug(sql)
+        self._cur.execute(sql)
+        return self._cur.fetchall()
+
+def split_db_connection(database_connection):
+    user = database_connection.split(':')[1].strip('/')
+    passwd = database_connection.split(':')[2].split('@')[0]
+    name = database_connection.split('/')[-1]
+    return (name,user,passwd)
 
 def send_report(sender,recipients,subject,message,
                 smtp_host='localhost',
@@ -65,8 +149,8 @@ def send_report(sender,recipients,subject,message,
 if __name__ == "__main__":
     # Process command line
     p = optparse.OptionParser(usage="%prog OPTIONS <recipient> [<recipient>...]")
-    p.add_option('-c',action="store",dest="galaxy_ini",default=None,
-                 help="get settings from GALAXY_INI configuration file")
+    p.add_option('-c',action="store",dest="galaxy_config",default=None,
+                 help="get settings from GALAXY_CONFIG file")
     p.add_option('-i',action="store",dest="interval",default="1 week",
                  help="report changes in time INTERVAL (e.g. '1 week')")
     p.add_option('-n',action="store_true",dest="no_email",
@@ -75,36 +159,23 @@ if __name__ == "__main__":
     # Send email report?
     send_email = (not opts.no_email)
     # Get database connection from config
-    if opts.galaxy_ini:
-        if not os.path.isfile(opts.galaxy_ini):
-            logging.critical("Config file '%s': not found" % opts.galaxy_ini)
+    if opts.galaxy_config:
+        if not os.path.isfile(opts.galaxy_config):
+            logging.critical("Config file '%s': not found" %
+                             opts.galaxy_config)
             sys.exit(1)
-        c = ConfigParser()
-        c.read(opts.galaxy_ini)
-        database_connection = c.get('app:main','database_connection')
-        brand = c.get('app:main','brand')
+        galaxy = GalaxyDatabase(opts.galaxy_config)
+        brand = galaxy.config.brand
         if send_email:
-            try:
-                smtp_server = c.get('app:main','smtp_server')
-                email_from = c.get('app:main','email_from')
-            except NoOptionError:
-                logging.warning("Email settings not found in ini file, email "
-                                "disabled")
+            smtp_server = galaxy.config.smtp_server
+            email_from = galaxy.config.email_from
+            if smtp_server is None or email_from is None:
+                logging.warning("Email settings not found in config file, "
+                                "email disabled")
                 send_email = False
     else:
-        logging.critical("Need to supply galaxy.ini file")
+        logging.critical("Need to supply galaxy config file")
         sys.exit(1)
-    logging.debug("Database connection: %s" % database_connection)
-    # Extract user, passwd and name
-    # FIXME this is horrible!
-    dbuser = database_connection.split(':')[1].strip('/')
-    dbpasswd = database_connection.split(':')[2].split('@')[0]
-    dbname = database_connection.split('/')[-1]
-    # Connect to database
-    conn = psycopg2.connect(database=dbname,
-                            user=dbuser,
-                            password=dbpasswd)
-    cur = conn.cursor()
     # Report user info
     print "%s: generating audit report" % time.strftime("%d/%m/%Y %H:%M:%S")
     interval = opts.interval
@@ -112,14 +183,14 @@ if __name__ == "__main__":
     report.append("Summary for %s" % time.strftime("%d/%m/%Y"))
     report.append('')
     report.append("In last %s:" % interval)
-    report.append("- New users    : %d" % len(users(cur,interval)))
-    report.append("- Jobs run     : %d" % len(jobs(cur,interval)))
-    report.append("- Jobs in error: %d" % len(jobs(cur,interval,'error')))
+    report.append("- New users    : %d" % len(galaxy.users(interval)))
+    report.append("- Jobs run     : %d" % len(galaxy.jobs(interval)))
+    report.append("- Jobs in error: %d" % len(galaxy.jobs(interval,'error')))
     report.append('')
     report.append("All time:")
-    report.append("- Registered users : %d" % len(users(cur)))
-    report.append("- Unactivated accts: %d" % len(users(cur,active=False)))
-    report.append("- Total jobs run   : %d" % len(jobs(cur)))
+    report.append("- Registered users : %d" % len(galaxy.users()))
+    report.append("- Unactivated accts: %d" % len(galaxy.users(active=False)))
+    report.append("- Total jobs run   : %d" % len(galaxy.jobs()))
     report.append('')
     if send_email:
         # Extract SMTP server information
@@ -134,4 +205,4 @@ if __name__ == "__main__":
     else:
         # Report job info
         print '\n'.join(report)
-    conn.close()
+    galaxy.close()
