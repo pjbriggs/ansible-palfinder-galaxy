9f18f2ad0c7c44a1c6165574e4e3904bd03780de
Peter Briggs <peter.briggs@manchester.ac.uk>
2024-11-08 11:58:23 +0000

    'galaxy' role: new utility script 'cleanup_dir.sh' for cleaning directories.

---
+++ roles/galaxy/files/cleanup_dir.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USAGE="$(basename $0) DIR INTERVAL"
+
+# Directory to clean up
+DIR=$1
+if [ -z "$DIR" ] ; then
+    echo "usage: $USAGE"
+    echo "ERROR: no directory specified" >&2
+    exit 1
+elif [ ! -d "$DIR" ] ; then
+    echo "ERROR: $DIR is not a directory" >&2
+    exit 1
+fi
+
+# Number of days after which files can be removed
+INTERVAL=$2
+if [ -z "$INTERVAL" ] ; then
+    echo "usage: $USAGE"
+    echo "ERROR: time interval (days) not specified" >&2
+    exit 1
+fi
+
+# Remove regular files
+find "${DIR}" -mindepth 2 -type f -mtime +${INTERVAL} -exec rm -rf {} \\;
+
+# Remove links
+find "${DIR}" -mindepth 2 -type l -mtime +${INTERVAL} -exec rm -rf {} \\;
+
+# Remove empty directories
+find "${DIR}" -mindepth 2 -type d -mtime +${INTERVAL} -empty -exec rmdir {} \\;
+
